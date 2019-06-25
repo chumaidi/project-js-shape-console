15b4f018d4ec866fc9520374b27a3d171bea8e56
chumaidi <chumaidihxc@gmail.com>
2019-06-25 15:07:56 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const rectangleWidth = 100
+const rectangleLength = 20
+
+const rectangleArea = rectangleWidth * rectangleLength
+
+console.log(`${rectangleArea} = ${rectangleWidth} * ${rectangleLength}`)
+
+
